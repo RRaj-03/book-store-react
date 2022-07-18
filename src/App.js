f9d436f5330df0905360d97c3b6c32ce75b94dd8
rraj-03 <ritikraj1369@gmail.com>
2022-07-19 04:17:33 +0530

    19-07-2022 draft 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Modal from './components/Modal';
-import ModalState from './context/ModalState';
+import ModalState from './context/modal/ModalState';
+import Cart from './components/Cart';
 import Content from './components/Content';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import CartState from './context/cart/CartState';
 function App() {
   
   return (
     <>
     <ModalState>
+    <CartState>
     <BrowserRouter>
     <Navbar />
     <Routes>
         <Route exact path="/" element={<Home />} />
         <Route  path="content/:search" element={<Content />} />
-        
+        <Route  path="cart" element={<Cart/>} />
     </Routes>
     <Modal/>
     <Footer/>
     </BrowserRouter> 
+    </CartState>
     </ModalState>  
     </>
   );
